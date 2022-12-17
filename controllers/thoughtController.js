6e1ba43ea228ef6e041e232436ead0d19a41d249
Alexis Brownstein <alexis@drdatabase.com>
2022-12-17 12:44:20 -0500

    more work on thoughtController.js to add reaction logic too

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -26,7 +26,7 @@ module.exports = {
   // create a new thought
   createThought(req, res) {
     Thought.create(req.body)
-      .then((student) => res.json(student))
+      .then((thought) => res.json(thought))
       .catch((err) => res.status(500).json(err));
   },
   // Delete a thought 
@@ -59,33 +59,34 @@ module.exports = {
 
   //alexis start here
 // create a new reaction
-createThought(req, res) {
+createReaction(req, res) {
   Thought.create(req.body)
-    .then((student) => res.json(student))
+    .then((reaction) => res.json(reaction))
     .catch((err) => res.status(500).json(err));
 },
 // Delete a reaction 
-deleteThought(req, res) {
+deleteReaction(req, res) {
   // attempt to find and delete a thought by id
-  Thought.findOneAndRemove({ _id: req.params.thoughtId })
-    .then((thought) =>
-      // if the thought is not found: return error message
-      !thought
-        ? res.status(404).json({ message: 'No such thought exists' })
-        // if thought is found: use the "thought.username" to find and update the user => "pull" the "thought.id" from the "thoughts" array
+  Reaction.findOneAndRemove({ _id: req.params.reactionId })
+    .then((reaction) =>
+      // if the reaction is not found: return error message
+      !reaction
+        ? res.status(404).json({ message: 'No such reaction exists' })
+        // // if thought is found: use the "thought.username" to find and update the user => "pull" the "thought.id" from the "thoughts" array
+        //alexis 12/17/22 we don't really n eed this below since already handled in thought but this is requiring :
         : User.findOneAndUpdate(
-            { username: thought.username },
-            { $pull: { thoughts: thought._id } },
+            { username: reaction.username },
+            { $pull: { reactions: reaction._id } },
             { new: true }
           )
     )
-    .then((user) =>
-      !user
-        ? res.status(404).json({
-            message: 'Thought deleted, but no user found',
-          })
-        : res.json({ message: 'Thought successfully deleted' })
-    )
+    // .then((user) =>
+    //   !user
+    //     ? res.status(404).json({
+    //         message: 'Thought deleted, but no user found',
+    //       })
+    //     : res.json({ message: 'Thought successfully deleted' })
+    // )
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
