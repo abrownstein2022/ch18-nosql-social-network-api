db7a807884d07fcad247dbf52760565a48beae68
Alexis Brownstein <alexis@drdatabase.com>
2022-12-20 13:47:12 -0500

    found better date-fns formatting for function

--- models/utils.js
+++ models/utils.js
@@ -1,6 +1,8 @@
 const dateFns = require('date-fns')
 function formatDate(dateValue){
-    return dateFns.format(dateValue, 'hh:ss MM/dd/y')
+  //from date-fns format list https://date-fns.org/v2.29.3/docs/format
+  /// Pp  04/29/1453, 12:00 AM
+    return dateFns.format(dateValue, 'Pp')
   }
 
 module.exports = {
